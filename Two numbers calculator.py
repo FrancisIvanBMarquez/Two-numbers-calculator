5b322197f14cbfac6dfe8b5f71f13f577d9ee81a
FrancisIvanBMarquez <francisivanmarquez@gmail.com>
2023-05-12 22:58:18 +0800

    ask two numbers working

--- Two numbers calculator.py
+++ Two numbers calculator.py
@@ -6,6 +6,8 @@ while try_again == 1:
     # choose operation / try catch
     operation = int(input("Enter 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division: "))
     # ask two numbers / try catch
+    first_number = float(input("Enter the first number: "))
+    second_number = float(input("Enter the second number: "))
     # result
     # update loop
     try_again = try_again - 1
