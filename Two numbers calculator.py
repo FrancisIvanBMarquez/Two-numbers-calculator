84f1e91ed107aa640c04772602d6081296b16fbc
FrancisIvanBMarquez <francisivanmarquez@gmail.com>
2023-05-12 23:14:09 +0800

    update loop and thank you working

--- Two numbers calculator.py
+++ Two numbers calculator.py
@@ -5,9 +5,11 @@ try_again = 1
 while try_again == 1:
     # choose operation / try catch
     operation = int(input("Enter 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division: "))
+
     # ask two numbers / try catch
     first_number = float(input("Enter the first number: "))
     second_number = float(input("Enter the second number: "))
+
     # result
     result = ""
     if operation == 1:
@@ -22,7 +24,11 @@ while try_again == 1:
     print("Your answer is: ", result)
 
         
-    # update loop
-    try_again = try_again - 1
-
-    # display thank you
+    # update loop / try catch
+    try_again_question = int(input("Do you want to try again? 1 for yes, 0 for no: "))
+    if try_again_question == 1:
+        pass
+    else:
+        try_again = try_again - 1
+        # display thank you
+        print("Thank you!")
