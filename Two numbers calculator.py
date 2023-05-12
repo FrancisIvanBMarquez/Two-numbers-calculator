f486fc8fdf5d74d4bf85b8580a85e69991db5748
FrancisIvanBMarquez <francisivanmarquez@gmail.com>
2023-05-12 22:48:10 +0800

    loop working

--- Two numbers calculator.py
+++ Two numbers calculator.py
@@ -1,8 +1,14 @@
 # Marquez, Francis Ivan B.,_BSCpE 1-5
 
 # loop
-# choose operation / try catch
-# ask two numbers / try catch
-# result
-# update loop
-# display thank you
+try_again = 1
+while try_again == 1:
+    print("a")
+    # choose operation / try catch
+    # ask two numbers / try catch
+    # result
+    # update loop
+    try_again = try_again - 1
+    print("b")
+
+    # display thank you
