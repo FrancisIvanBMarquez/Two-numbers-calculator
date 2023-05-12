9d042230dc981aff43a5d54f8c3b0ef328b6404b
FrancisIvanBMarquez <francisivanmarquez@gmail.com>
2023-05-12 22:52:57 +0800

    operation input working

--- Two numbers calculator.py
+++ Two numbers calculator.py
@@ -3,12 +3,11 @@
 # loop
 try_again = 1
 while try_again == 1:
-    print("a")
     # choose operation / try catch
+    operation = int(input("Enter 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division: "))
     # ask two numbers / try catch
     # result
     # update loop
     try_again = try_again - 1
-    print("b")
 
     # display thank you
